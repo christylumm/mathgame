722dfb3eec7b89d697dff9518ad13fb1a2262440
Christy Lum <christy.lum121@gmail.com>
2020-09-14 19:01:53 +0000

    initial commit

---
+++ main.rb
@@ -0,0 +1,24 @@
+# # PLAYER
+# need to keep track of: 
+# - initial number of lives
+# - player name
+
+# # GAME
+# need to keep track of: 
+# - lives left
+# - players
+# - current player
+
+# methods: 
+# - initialize the players 
+# - subtract life
+# - record lives left
+# - change player
+
+# # QUESTION
+# need to keep track of: 
+# - answers
+
+# methods: 
+# - generate questions
+
